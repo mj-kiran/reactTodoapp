c3337d3c5fce4b81b8749292cb23fd6de4bd6f34
mj-kiran <kiranmjoshua@gmail.com>
2023-06-25 11:27:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {useState}from 'react'
+
 
 function App() {
+  const [todos,setTodos]=useState([])
+  const [toDo,setTodo]=useState('')
+  console.log(todos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDo}onChange={(event)=>setTodo(event.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setTodos([...todos,{id:Date.now() ,text:toDo , status:false}])} className="fas fa-plus"></i>
+        
+      </div>
+    
+      <div className="todos">
+        {todos.map((obj)=>{
+          return(
+          <div className="todo">
+           <div className="left">
+             <input onChange={(e)=>{
+              console.log(e.target.checked);
+              console.log("obj",obj);
+              setTodos(todos.filter(obj2=>{
+                if(obj2.id===obj.id){
+                  obj2.status=e.target.checked
+                }
+                return obj2
+              }
+          
+              ))
+             }} value={obj.status} type="checkbox" name="" id="" />
+             <p>{obj.text}</p>
+           </div>
+           <div className="right">
+             <i className="fas fa-times"></i>
+           </div>
+         </div>
+         )
+
+        })
+          
+         
+        
+        }
+        {
+          todos.map((obj)=>{
+            if(obj.status===true){
+              return(<h1>{obj.text}</h1>)
+            }
+            return null
+          })
+        }
+      </div>
     </div>
   );
 }
